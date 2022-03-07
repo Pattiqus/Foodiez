75a253a82bf0492e4eeaead09c7da770828ffaf3
Mohammed Mazahim <mazamim91@gmail.com>
2022-03-08 00:05:34 +1100

    design basic

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+const modal= document.querySelector("#myModals")
+
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+    console.log("ok");
+
+
+  }
+  
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
+
+
+
+
+
